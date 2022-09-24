6afe2268160f97ddcc59ef95aee239fe259ccd16
Alysson Renan <alyssonrenacc@gmail.com>
2022-09-24 17:51:09 -0300

    add js in display mobile

--- js/script.js
+++ js/script.js
@@ -1,10 +1,14 @@
 const panel = document.getElementById('container')
 const btnRegister =  document.getElementById('btn-register')
 const btnLogin =  document.getElementById('btn-login')
-
+const loginMobile = document.getElementById('login-mobile')
+const registerMobile = document.getElementById('register-mobile')
 
 const moveCard = () => panel.classList.toggle('move-card')
 
+loginMobile.addEventListener('click', moveCard)
+registerMobile.addEventListener('click', moveCard)
 
 btnRegister.addEventListener('click', moveCard)
 btnLogin.addEventListener('click', moveCard)
+
